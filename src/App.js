077530cf21a8092b8cfefeba661130382ddf032f
Sumin Ha <minha62@ewhain.net>
2024-05-18 17:01:07 +0900

    Create todos

--- src/App.js
+++ src/App.js
@@ -11,12 +11,30 @@ import api from "./utils/api"
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
 
   const getTasks = async () => {
     const response = await api.get('/tasks');
     console.log("repsone", response);
     setTodoList(response.data.data);
   };
+  const addTask = async () => {
+    try {
+      const response = await api.post('/tasks', {
+        task: todoValue,
+        isComplete: false,
+      });
+      if (response.status === 200) {
+        console.log('Success');
+        setTodoValue(""); // 입력창 초기화
+        getTasks(); // 값 추가하면 다시 할일리스트 출력
+      } else {
+        throw new Error('Task cannot be added');
+      }
+    } catch (err) {
+      console.log('Error', err);
+    }
+  }
   useEffect(() => {
     getTasks();
   }, []);
@@ -28,10 +46,12 @@ function App() {
             type="text"
             placeholder="할일을 입력하세요"
             className="input-box"
+            value={todoValue}
+            onChange={(event) => setTodoValue(event.target.value)}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add">추가</button>
+          <button className="button-add" onClick={addTask}>추가</button>
         </Col>
       </Row>
 
